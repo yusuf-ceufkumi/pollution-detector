61174f2e47addbd62b8e946259ecdfefdf2ff535
Yusuf <myyusufdev01@gmail.com>
2023-09-27 23:03:10 +0700

    Initial commit

---
+++ util.py
@@ -0,0 +1,20 @@
+import yaml
+import joblib
+from datetime import datetime
+
+config_dir = "config/config.yaml"
+
+def time_stamp() -> datetime:
+    # Return current date and time
+    return datetime.now()
+
+def load_config() -> dict: 
+    # Try to load yaml file
+    try:
+        with open(config_dir, "r") as file:
+            config = yaml.safe_load(file)
+    except FileNotFoundError as fe:
+        raise RuntimeError("Parameters file not found in path.")
+
+    # Return params in dict format
+    return config
